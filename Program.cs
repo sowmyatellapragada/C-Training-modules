84413703e2de35592a773d1af15a100db919bbc5
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2020-11-26 13:46:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreatBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Sowmya grade book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            var stats=book.GetStatistics();
+            Console.WriteLine($"The lowest grade is{stats.lowgrade}");
+            Console.WriteLine($"The high grade is{stats.highgrade}");
+            Console.WriteLine(value: $"The average grade is{stats.Average:N1}");
+            
+            
+            
+
+       
+    }
+}
+}
---
+++ Statistics.cs
@@ -0,0 +1,9 @@
+namespace GreatBook
+{
+    public class Statistics{
+        public double Average;
+        public double highgrade;
+        public double lowgrade;
+    }
+        
+}
